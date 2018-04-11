9b3c3a59191317a47c592f1f981d02bf56ee7592
henzier <henzier@oregonstate.edu>
2018-04-10 17:58:12 -0700

    Fixed a couple of things and added Y
    Made sure that lineWords was not commented out, made sure that X was made to correct number of columns (13 without dummy column).  Also Y now is populated with the average value of each example (last value of each line).

--- p1.py
+++ p1.py
@@ -1,19 +1,27 @@
 import numpy as np
 
-X = np.zeros(shape=(1,14))
+X = np.zeros(shape=(1,13)) # 13 if without dummy column, else 14.
+Y = np.zeros(shape=(1,1))
+
 firstLine = True
 with open('housing_train.txt','r') as f:
     for line in f:
     	featureNum = 0
+    	lineWords = []
+    	averageValue = []
     	# lineWords = [1.] # used to add a 1s to the first column (dummy variable)
         for word in line.split():
-
            if featureNum < 13:
            	lineWords.append(word)
            	featureNum += 1
+           else:
+           	averageValue.append(word)
         if firstLine:
         	X[0] = lineWords
+        	Y[0] = averageValue
         	firstLine = False
         else:
         	X = np.vstack((X, lineWords))
-print X
+        	Y = np.vstack((Y, averageValue))
+print X #for testing purposes
+print Y[-1] # for testing purposes

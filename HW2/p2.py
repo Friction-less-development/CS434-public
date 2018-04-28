67aa321ed1420af3e84479a35200b69087bdf01b
henzier <henzier@oregonstate.edu>
2018-04-27 17:54:06 -0700

    Calculates H(S) correctly
    It now calculates H(S) correctly (which only has to be done once for the training, because number of positives/negatives won't change for the root, because root incorporates the entire amount of data), see slide 19.
    
    Going to start finding the best decision stump (for column 1), before trying to the find the best decision stump overall (over all columns), using the equations found on slide 19.

--- HW2/p2.py
+++ HW2/p2.py
@@ -44,6 +44,22 @@ with open('knn_train.csv','r') as f:
         else:
         	X = np.vstack((X, lineWords))
 
+#global
+bestStump = 0.0 # global, will be used to find the best overall decision stump
+hS = 0.0 # H(S) value, will be the same for no matter which column it is, because the number of positives/negatives overall will not change
+hSList = [] # list of h(s1), h(s2)... will be used later
+
+# note that this is a per column basis
+greaterThan = 0.0 # will be used to where the cutoff is, it will go anything greater than will become "+" while evertyhing less than or equal to will become "-", as shown in slide 19
+positiveList = [] # overall column positive list
+negativesList = [] # overall column negative list
+lBranch = [] # used to store if something is negative/positive in left branch
+rBranch = [] # used to store if something is negative/positive in right branch
+rightBranch = [] # will be used to hold everything greater than greaterThan value
+leftBranch = []  # will be used to hold everything less than or equal to greaterThan value
+posNumCount = -1
+negNumCount = -1
+labelBranch = -1
 print X
 # print Y
 print "\n"
@@ -53,27 +69,105 @@ print XSortTest
 print "\n"
 print Y
 
-#global
-bestStump = 0.0 # global, will be used to find the best overall decision stump h(s)
-hSList = [] # list of h(s1), h(s2)... will be used later
-
-# note that this is a per column basis
-greaterThan = 0.0 # will be used to where the cutoff is, it will go anything greater than will become "+" while evertyhing less than or equal to will become "-", as shown in slide 19
-positiveList = [] # will be used to hold everything greater than greaterThan value
-negativesList = []  # will be used to hold everything less than or equal to greaterThan value
-
-negativesList.append(Y[0])
-greaterThan = Y[0]
-for i in range(1, np.size(Y)):
-	if Y[i] > greaterThan:
-		positiveList.append(Y[i])
+for i in range(0, np.size(Y)):
+	if X[i][0] == 1:
+		positiveList.append(float(Y[i]))
 	else:
-		negativesList.append(Y[i])
+		negativesList.append(float(Y[i]))
 
-formattedPositiveList = [ '%.2f' % elem for elem in positiveList]
-formattedNegativeList = [ '%.2f' % elem for elem in negativesList]
-print formattedPositiveList
-print "Number of positives: ", np.size(positiveList)
-print "\n", formattedNegativeList
-print "Number of negatives: ", np.size(negativesList)
-print "\n", greaterThan
+print "Total number of positives: ", np.size(positiveList)
+print "Total number of negatives: ", np.size(negativesList)
+tPosONegatives = np.size(positiveList)/float(np.size(negativesList)+np.size(positiveList)) # positives/negatives
+tNegOPositives = np.size(negativesList)/float(np.size(positiveList)+np.size(negativesList)) # negatives/positives
+hS = -tPosONegatives*np.log2(tPosONegatives)-tNegOPositives*np.log2(tNegOPositives)
+print "H(S): ", hS
+#column 1
+# for j in range(0, np.size(Y)-1):
+# 	greaterThan = Y[j]
+# 	for i in range(0, np.size(Y)):
+# 		if Y[i] > greaterThan:
+# 			rightBranch.append(Y[i])
+# 			if X[i][0] == 1:
+# 				rBranch.append(1)
+# 			else:
+# 				rBranch.append(-1)
+# 		else:
+# 			leftBranch.append(Y[i])
+# 			if X[i][0] == 1:
+# 				lBranch.append(1)
+# 			else:
+# 				lBranch.append(-1)
+# 		# if X[i][0] == 1:
+# 		# 	positiveList.append(float(Y[i]))
+# 		# else:
+# 		# 	negativesList.append(float(Y[i]))
+
+# 	# formattedPositiveList = [ '%.2f' % elem for elem in positiveList]
+# 	# formattedNegativeList = [ '%.2f' % elem for elem in negativesList]
+# 	# print formattedPositiveList
+# 	# print "Number of positives: ", np.size(positiveList)
+# 	# print "\n", formattedNegativeList
+# 	# print "Number of negatives: ", np.size(negativesList)
+# 	# print "\n", greaterThan
+# 	tempLabelBranch = -1
+# 	if np.size(positiveList) > np.size(negativesList):
+# 		tempLabelBranch = 1
+# 	else:
+# 		tempLabelBranch = 0
+# 	if np.size(hSList) == 0:
+# 		posONegatives = np.size(positiveList)/float(np.size(negativesList)+np.size(positiveList)) # positives/negatives
+# 		negOPositives = np.size(negativesList)/float(np.size(positiveList)+np.size(negativesList)) # negatives/positives
+# 		temp = -posONegatives*np.log2(posONegatives)-negOPositives*np.log2(negOPositives)
+# 		posNumCount = np.size(positiveList)
+# 		negNumCount = np.size(negativesList)
+# 		if(posNumCount > negNumCount):
+# 			labelBranch = 1
+# 		else:
+# 			labelBranch = 0
+# 		hSList.append(temp)
+# 	elif tempLabelBranch != labelBranch:
+# 		posONegatives = np.size(positiveList)/float(np.size(negativesList)+np.size(positiveList)) # positives/negatives
+# 		negOPositives = np.size(negativesList)/float(np.size(positiveList)+np.size(negativesList)) # negatives/positives
+# 		temp = -posONegatives*np.log2(posONegatives)-negOPositives*np.log2(negOPositives)
+# 		if temp < hSList[0]: # 0 would i, in some loop for each column/feature
+# 			hSList[0] = temp
+
+# 	# print hSList
+# 	# del hSList[:] # used to empty a list
+# 	# del negativesList[:]
+# 	# del positiveList[:]
+
+# print hSList
+
+# greaterThan = Y[1]
+# for i in range(0, np.size(Y)):
+# 	if Y[i] > greaterThan:
+# 		positiveList.append(Y[i])
+# 	else:
+# 		negativesList.append(Y[i])
+
+# # formattedPositiveList = [ '%.2f' % elem for elem in positiveList]
+# # formattedNegativeList = [ '%.2f' % elem for elem in negativesList]
+# # print formattedPositiveList
+# # print "Number of positives: ", np.size(positiveList)
+# # print "\n", formattedNegativeList
+# # print "Number of negatives: ", np.size(negativesList)
+# # print "\n", greaterThan
+# if np.size(hSList) == 0:
+# 	posONegatives = np.size(positiveList)/float(np.size(negativesList)+np.size(positiveList)) # positives/negatives
+# 	negOPositives = np.size(negativesList)/float(np.size(positiveList)+np.size(negativesList)) # negatives/positives
+# 	temp = -posONegatives*np.log2(posONegatives)-negOPositives*np.log2(negOPositives)
+# 	posNumCount = np.size(positiveList)
+# 	negNumCount = np.size(negativesList)
+# 	hSList.append(temp)
+# elif np.size(positiveList) != posNumCount and np.size(negativesList) != negNumCount:
+# 	posONegatives = np.size(positiveList)/float(np.size(negativesList)+np.size(positiveList)) # positives/negatives
+# 	negOPositives = np.size(negativesList)/float(np.size(positiveList)+np.size(negativesList)) # negatives/positives
+# 	temp = -posONegatives*np.log2(posONegatives)-negOPositives*np.log2(negOPositives)
+# 	if temp < hSList[0]: # 0 would i, in some loop for each column/feature
+# 		hSList[0] = temp
+
+# print hSList
+# # del hSList[:] # used to empty a list
+# del negativesList[:]
+# del positiveList[:]

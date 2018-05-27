a2023445adfabd90d42041a0854590644d7a66a1
henzier <henzier@oregonstate.edu>
2018-05-26 21:40:33 -0700

    Basically working problem 2
    Part 1, 2 and 3 seem to be working.  We do need to change it a little to make sure it is easy to see the eigen-values (for part 1 of problem, as it needs to be reported).

--- HW4/p2.py
+++ HW4/p2.py
@@ -4,7 +4,8 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-#source: http://sebastianraschka.com/Articles/2014_pca_step_by_step.html
+#sources: 	http://sebastianraschka.com/Articles/2014_pca_step_by_step.html
+#			https://stackoverflow.com/questions/13224362/principal-component-analysis-pca-in-python
 
 X = np.zeros(shape=(1, 784))
 
@@ -24,73 +25,22 @@ with open('data-1.txt','r') as f:
             if(lines%100 == 0):
             	print lines
             lines += 1
+meanX = np.zeros(shape=(784, 1))
+meanX = np.mean(X, axis=0)
+X = X - meanX
 
-mean_list = []
-cov_matrix = np.zeros(shape=(1, 784))
-for i in range(0, 784):
-	mean_list.append(np.mean(X[i, :]))
+R = np.cov(X, rowvar=False)
+from scipy import linalg as LA
 
-for i in range(0, 784):
-	if(i==0):
-		cov_matrix[0] = X[i, :]
-	else:		
-		cov_matrix = np.vstack((cov_matrix, X[i, :]))
+evals, evecs = LA.eig(R)
+idx = np.argsort(evals)[::-1]
+evecs = evecs[:,idx]
 
-mean_vector = np.array(mean_list)
-#print("Mean Vector:\n", mean_vector)
-cov_mat = np.cov(cov_matrix)
-print np.shape(cov_mat)
-#print("Covariance Matrix:\n", cov_mat)
-plt.title("Mean Image")
-
-plt.imshow(np.reshape(mean_vector,(28,28)))
-plt.savefig("mean.png")
-
-eig_val_cov, eig_vec_cov = np.linalg.eig(cov_mat)
-# for i in range(len(eig_val_cov)):
-# 	eigvec_cov = eig_vec_cov[:,i].reshape(1,6000).T
+evals = evals[idx]
+evecs = evecs[:, :10]
+transformed = evecs.real
 
 
-# 	print('Eigenvector {}: \n{}'.format(i+1, eig_vec_cov))
-# 	print('Eigenvalue {} from covariance matrix: {}'.format(i+1, eig_val_cov[i]))
-# 	print(40 * '-')
-
-# Make a list of (eigenvalue, eigenvector) tuples
-
-eig_pairs = [(np.abs(eig_val_cov[i]), eig_vec_cov[:,i]) for i in range(len(eig_val_cov))]
-print np.shape(eig_pairs[0][1])
-topEigenVals = []
-counter = 0;
-
-# Sort the (eigenvalue, eigenvector) tuples from high to low
-eig_pairs.sort(key=lambda x: x[0], reverse=True)
-print "Top 10 eigen values: "
-for i in eig_pairs:
-	if(counter==10):
-		break
-	print(i[0])
-	topEigenVals.append(i[0])
-	counter += 1
-
-print "\n"
-#for i in range(0, 10):
-	#np.multiply(eig_pairs[i][0], np.amax(eig_pairs[i][0]))
-
-matrix_w = np.hstack((eig_pairs[0][1].reshape(784,1), eig_pairs[1][1].reshape(784,1), eig_pairs[2][1].reshape(784,1), eig_pairs[3][1].reshape(784,1), eig_pairs[4][1].reshape(784,1), eig_pairs[5][1].reshape(784,1), eig_pairs[6][1].reshape(784,1), eig_pairs[7][1].reshape(784,1), eig_pairs[8][1].reshape(784,1), eig_pairs[9][1].reshape(784,1)))
-
-print('Matrix W:\n', matrix_w)
-print np.shape(matrix_w)
-print np.shape(topEigenVals)
-#transformed = np.zeros(shape=(10, 784))
-#transformedT = matrix_w.T.dot(X)
-for i in range(0, 10):
-	for j in range(0, 784):
-		matrix_w[j][i] = abs(matrix_w[j][i] * topEigenVals[i])
-
-transformed = matrix_w.real
-
-print('Matrix W changed:\n', transformed)
-#print transformed
 fileName = "eigen"
 fileExtension = ".png"
 for i in range(0, 10):
@@ -103,8 +53,30 @@ for i in range(0, 10):
 	fileSave += fileExtension
 	plt.savefig(fileSave)
 
-# print X
-# plt.figure(1)
-# plt.title("Test Picture")
-# plt.imshow(np.reshape(X[0],(28,28)))
-# plt.savefig("p2.png")
+
+transformedX = np.dot(evecs.T, X.T).T
+
+print( transformedX.shape)
+
+closestFittingTracker = np.zeros(shape=(10, 1))
+closestFitting = np.zeros(shape=(10, 1))
+
+for i in range(0, 6000):
+	for j in range(0, 10):
+		if transformedX[i, j] > closestFittingTracker[j, 0]:
+			closestFittingTracker[j, 0] = transformedX[i, j]
+			closestFitting[j,0] = i
+print(closestFittingTracker)
+print(closestFitting)
+
+fileName = "eigenmatch"
+fileExtension = ".png"
+for i in range(0, 10):
+	fileTitle = "Eigenmatch"
+	fileTitle += str(i)
+	plt.title(fileTitle)
+	plt.imshow(np.reshape(X[int(closestFitting[i]), : ],(28,28)))
+	fileSave = fileName
+	fileSave += str(i)
+	fileSave += fileExtension
+	plt.savefig(fileSave)

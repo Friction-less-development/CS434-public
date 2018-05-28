619a6271aa9456150dfe7f053b6a39717a348589
Michael <giovanmi@oregonstate.edu>
2018-05-27 21:28:24 -0700

    updated p1

--- HW4/p1.py
+++ HW4/p1.py
@@ -1,4 +1,4 @@
-
+#Authors: Rex Henzie, Benjamin Richards, and Michael Giovannoni
 
 import numpy as np
 import matplotlib as mpl
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from random import randint
 import math
-
-
-# In[9]:
 
 
 X = np.zeros(shape=(1, 784))
@@ -28,56 +25,33 @@ with open('data-1.txt','r') as f:
         else:
             X = np.vstack((X, lineWords))
 #             if(lines%100 == 0):
-#             	print (lines
+#               print (lines
             lines += 1
 
-
-# In[10]:
 
 
 print(X.shape)
 
-
-# In[47]:
-
-
-def kmeans(data, k):
+# k-means function
+def kmeans(data, k, iterations):
+    print("Starting K-means.\n K = ", k, "\n")
+    
     # start with k random centers chosen from our data set
     rows, columns = data.shape
     centers = []
     SSElist = []
     for s in range (0, k):
         centers.append(data[randint(0,rows)])
-    print("Starting center list length: ", len(centers))
-#     print(centers)
     
     previousLabels = []
     currentLabels = []
     iters = 0
     notConverged = True
     
-    while notConverged:
+    while iters <= iterations - 1:
         previousLabels = currentLabels
         currentLabels = calcClusters(centers, data)
-        
-#         print("currentLabels: ", currentLabels)
-#         print("\n")
-#         print("previousLabels: ", previousLabels)
-#         print("\n")
-        
-        # check to make sure the labels aren't the same (it's converged)
-        if previousLabels == currentLabels:
-            print("Converged after ", iters, " iterations")
-            return
-        
-#         if iters%5 == 0:
-#             print(iters, " iterations\n")
-#             for s in range (0, k):
-#                 print(np.linalg.norm(centers[s]), " ")
-        
-        
-               
-        
+
         # recompute centers to be the mean of each vector labeled as belonging to that cluster 
         for n in range(0, k):
             indicesBelongingToCluster = np.where(np.array(currentLabels) == n)
@@ -87,11 +61,12 @@ def kmeans(data, k):
         
         #compute SSE
         SSElist.append(computeSSE(centers, data, currentLabels))
-        print( "Iteration: ", iters, " ", SSElist[iters], "\n")
+#         print( "Iteration: ", iters, " ", SSElist[iters], "\n")
         
         iters += 1
         
-    return
+    print("\n Done")
+    return SSElist
 
 # compute SSE
 def computeSSE(centers, data, labels):
@@ -102,9 +77,9 @@ def computeSSE(centers, data, labels):
         clusterData = data[indicesBelongingToCluster]
         
         for s in clusterData:
-            indvClusterSSE.append(np.linalg.norm(centers[n]-s))
+            indvClusterSSE.append(np.sqrt((np.linalg.norm(centers[n]-s)**2)))
     
-    return sum(indvClusterSSE)
+    return round(sum(indvClusterSSE))
     
 # calculates the closest center for each sample, returns a list of sample labels
 def calcClusters(centers, samples):
@@ -135,9 +110,67 @@ def calcBestCluster(centers, sample):
     return bestCenterIdx
         
     
+
+
+multSSE = []
+iterations = 10
+maxK = 20
+maxK += 1
+for k in range(2, maxK):
+#     print("K = ", k, "\n")
+    SSElist = kmeans(X, k, iterations)
+    multSSE.append(SSElist)
     
 
+
+labels = []
+for k in range(2, maxK):
+    labels.append("K = " + str(k))
+
+myX = range(1, iterations+1)
+
+plt.figure(figsize=(10,8))
+plt.ylabel('SSE')
+plt.xlabel('iterations')
+plt.title('K-Means Error')
+
+for k in range(len(multSSE)):
+    plt.plot(myX, multSSE[k])
     
-kmeans(X, 40)
+
+plt.legend(labels)
+plt.show()
+# plt.savefig('p1k2i20.png')
+
+
+myX = range(2, maxK)
+plt.figure(figsize=(10,8))
+plt.ylabel('SSE')
+plt.xlabel('# of clusters')
+plt.title('SSE compared to K')
+kYvector = []
+print("!!!!!!", iterations, " ", maxK)
+for t in range(len(multSSE)):
+#     print("t = ", t, "iterations = ", iterations, "\n")
+    kYvector.append(multSSE[t][iterations-1])
     
+    
+plt.plot(myX, kYvector)
+plt.show()
+
+
+
+iterations = 10
+k = 2
+SSElist = kmeans(X, k, iterations)
+
+label = "K = " + str(k)
+plt.figure(figsize=(10,8))
+plt.ylabel('Sum of Errors')
+plt.xlabel('iterations')
+plt.plot(myX, SSElist, color="blue", label=label)
+plt.title('K-Means Error')
+plt.legend()
+plt.show()
+# plt.savefig('p1k2i20.png')
 

4593f980d5d9b1f4fa501cbcc369d06601d01e6b
henzier <henzier@oregonstate.edu>
2018-06-07 21:14:47 -0700

    Got it sorta working (for one subject)
    It seems to being doing something for a single subject, I need to change it though to work with all the training subject; I haven't ran this on the general pop tests yet, as I suspect it will be ridiculously low (one subject, and not much with figuring out what decision tree values to tweak, such as depth, number of leaves, number of features, etc.)  
    
    I also need to play with which columns might need to be removed.  I am pretty sure the time (column 0) will need to be removed, because I believe it's possible it might be leading to over fitting. (We'll see about it).

--- final_project/fp1_tree.py
+++ final_project/fp1_tree.py
@@ -10,12 +10,14 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Sources: 
 #           http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+#           https://stackoverflow.com/questions/42757892/how-to-use-warm-start
 
 
-instance = np.zeros(shape=(7, 8))
-chunks = np.zeros(shape=(1, 8))
+instance = np.zeros(shape=(7, 8)) # normally 8, might want to consider 7 and skip the first time column, because of time difference might be triggering it to think it is a good indicator
+chunks = np.zeros(shape=(1, 8)) # normally 8, might want to consider 7 and skip the first time column, because of time difference might be triggering it to think it is a good indicator
 chunksList = []
-SUB1 = np.zeros(shape=(1, 8))
+sub1HypoChunkList = []
+SUB1 = np.zeros(shape=(1, 8)) # normally 8, might want to consider 7 and skip the first time column, because of time difference might be triggering it to think it is a good indicator
 SUB1HYPO = np.zeros(shape=(1,1)) # this is whether there will be a hypo in 30 minutes or not (what we're trying to predict)
 SUB1INDICES = np.zeros(shape=(1,1)) # this will hold the indices for subject 1
 
@@ -58,47 +60,94 @@ with open('list_1.csv','r') as f:
 
 counter = 0 # must get to at least equaling 6, with a minimum of 7 instances for a 30 minute period
 
-for i in range (0, np.size(SUB1,0)):
-	if counter == 0:
-		chunks[0] = SUB1[i][:]
-		counter += 1
-	else:
-		chunks = np.vstack((chunks, SUB1[i][:]))
-		counter += 1
-	if i != 0:
-		if SUB1INDICES[i][0] - 1 != SUB1INDICES[i-1][0] and counter < 6:
-			temp = np.zeros(shape=(1, 8))
-			chunks = temp
-			counter = 0
-		elif SUB1INDICES[i][0] - 1 != SUB1INDICES[i-1][0] and counter >= 6:
-			chunksList.append(chunks)
-			temp = np.zeros(shape=(1, 8))
-			chunks = temp
-			counter = 0
+sub1Forest = RandomForestClassifier(criterion="entropy", max_depth=7, random_state=1, warm_start=True)
 
-for chunkI in chunksList:
+# have a problem, need to get SUB1HYPO, aka the training 0 and 1s, to line up with these chunks, will have to probably create a list of SUB1HYPO chunks that correspond to each respective chunk
+sub1HypoChunk = np.zeros(shape=(1,1))
+for i in range (0, np.size(SUB1,0)):
+    if counter == 0:
+        sub1HypoChunk[0] = SUB1HYPO[i][:]
+        chunks[0] = SUB1[i][:]
+        counter += 1
+    else:
+        chunks = np.vstack((chunks, SUB1[i][:]))
+        sub1HypoChunk = np.vstack((sub1HypoChunk, SUB1HYPO[i][:]))
+        counter += 1
+    if i != 0:
+        if SUB1INDICES[i][0] - 1 != SUB1INDICES[i-1][0] and counter < 6:
+            temp = np.zeros(shape=(1, 8))
+            chunks = temp
+            counter = 0
+            temp2 = np.zeros(shape=(1,1))
+            sub1HypoChunk = temp2
+        elif SUB1INDICES[i][0] - 1 != SUB1INDICES[i-1][0] and counter >= 6:
+            sub1HypoChunkList.append(sub1HypoChunk)
+            chunksList.append(chunks)
+            temp = np.zeros(shape=(1, 8))
+            chunks = temp
+            temp2 = np.zeros(shape=(1,1))
+            sub1HypoChunk = temp2
+            counter = 0
+
+# print np.shape(chunksList)
+# print len(chunksList)
+counter = 0
+for i in range(0, len(chunksList)):
+    temp2 = np.zeros(shape=(7,1))
+    sub1HypoChunk = temp2
     temp = np.zeros(shape=(7, 8))
     instance = temp
-    for j in range(0, len(chunkI)):
+    for j in range(0, len(chunksList[i])):
+        if j==len(chunksList[i])-1:
+            print i
+            # print j
+            # print instance
+            # print sub1HypoChunk
+            # print 5*"-"
         if j<7:
-            instance[j][:] = chunkI[j][:]
+            instance[j][:] = chunksList[i][j][:]
+            sub1HypoChunk[j][0] = sub1HypoChunkList[i][j][0]
         else:
+            # for k in range(0, np.size(instance, 0)):
+            #     for l in range(0, 8):
+            sub1Forest.fit(instance, np.ravel(sub1HypoChunk))
+            sub1Forest.n_estimators += 1
             tempNum = j%7-1
             instance = np.delete(instance, tempNum, 0)
-            instance = np.vstack((instance, chunkI[j][:]))
+            sub1HypoChunk = np.delete(sub1HypoChunk, tempNum, 0)
+            instance = np.vstack((instance, chunksList[i][j][:]))
+            sub1HypoChunk = np.vstack((sub1HypoChunk, sub1HypoChunkList[i][j][0]))
 
-print np.shape(instance)
-print np.shape(chunks)
-print np.shape(chunksList)
-
+# print sub1HypoChunkList[0][349]
+# print sub1HypoChunkList[0][348]
+# print np.shape(sub1HypoChunkList)
+# print np.shape(instance)
+# print np.shape(chunks)
+print "\n"
 print "Subject_1"
 print np.shape(SUB1)
 print np.shape(SUB1HYPO)
 print np.shape(SUB1INDICES)
 
-sub1Forest = RandomForestClassifier(max_depth=2, random_state=0)
-sub1Forest.fit(SUB1, np.ravel(SUB1HYPO))
-print sub1Forest.feature_importances_
+# sub1Forest.fit(SUB1, np.ravel(SUB1HYPO))
+# print sub1Forest.feature_importances_
+importances = sub1Forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in sub1Forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+for f in range(SUB1.shape[1]):
+    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+# plt.figure()
+# plt.title("Feature importances")
+# plt.bar(range(SUB1.shape[1]), importances[indices],
+#        color="r", yerr=std[indices], align="center")
+# plt.xticks(range(SUB1.shape[1]), indices)
+# plt.xlim([-1, SUB1.shape[1]])
+# plt.savefig('FeatureImportance.png')
+
+
+
 
 SUB4 = np.zeros(shape=(1, 8))
 SUB4HYPO = np.zeros(shape=(1,1)) # this is whether there will be a hypo in 30 minutes or not (what we're trying to predict)
